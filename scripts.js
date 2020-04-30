c20df0cc41d1b37bfa39f1ed462d50e063c19c8d
zmarton81 <martonzoltanandras@gmail.com>
2020-04-30 21:35:14 +0200

    removed add button

--- scripts.js
+++ scripts.js
@@ -1,1 +1,53 @@
-console.log("Script file is loaded")
+/* $('css selector').action()*/
+console.log("Script file is loaded");
+
+$(`h1`).text(`This is done by Javascript!`);
+$(`h1`).html(`This is done by <em>Javascript!</em>`);
+/* $('h2').css('color', 'red');*/
+$('h2').toggleClass('blue');
+
+
+function mylittlefunction() {
+    if ($('p').length <5) {
+        $('main').append("<p>A p is appended in the main.</p>");    
+    }
+    else {
+        $('button').remove('#add');
+        
+        /*console.log('we reached the maximum paragraph number');*/
+    }
+    $('p').css(`border`, `1px solid Red`);
+    $('p').css('padding', '10px');   
+    }
+
+/*function mylittlefunction() {
+    $('main').append("<p>A p is appended in the main.</p>");    
+    $('p').css(`border`, `1px solid Red`);
+    $('p').css('padding', '10px');
+
+} */
+
+/*how to use a funciton */
+/*mylittlefunction(); 
+mylittlefunction(); */
+
+$('#add').click(mylittlefunction); 
+$('#remove').click(function() {
+ 
+    $('p:last-child').remove();
+  
+   }
+)
+
+
+
+ 
+
+
+// fourth part:
+
+// if (condition that true or false) {
+    // condition is true
+// } else {
+    // condition is false 
+// }
